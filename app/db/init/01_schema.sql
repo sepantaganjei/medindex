5cf56fa252051f9d969a3796efc966d6f0de3094
steve <steven.mor@mail.polimi.it>
2026-05-21 14:42:29 +0200

    Adjusted the DB schema

--- app/db/init/01_schema.sql
+++ app/db/init/01_schema.sql
@@ -1,3 +1,4 @@
+-- collections
 CREATE TABLE IF NOT EXISTS collections (
     name VARCHAR PRIMARY KEY,
     description VARCHAR,
@@ -6,6 +7,7 @@ CREATE TABLE IF NOT EXISTS collections (
     description_uri VARCHAR
 );
 
+-- patients
 CREATE TABLE IF NOT EXISTS patients (
     id VARCHAR PRIMARY KEY,
     sex VARCHAR,
@@ -13,6 +15,7 @@ CREATE TABLE IF NOT EXISTS patients (
     ethnic_group VARCHAR
 );
 
+-- studies
 CREATE TABLE IF NOT EXISTS studies (
     instance_uid VARCHAR PRIMARY KEY,
     collection VARCHAR REFERENCES collections(name),
@@ -21,14 +24,16 @@ CREATE TABLE IF NOT EXISTS studies (
     description VARCHAR,
     series_count INTEGER,
     patient_id VARCHAR REFERENCES patients(id),
-    "LongitudinalTemporalEventType" VARCHAR,
-    "LongitudinalTemporalOffsetFromEvent" VARCHAR
+    longitudinal_temporal_event_type VARCHAR,
+    longitudinal_temporal_offset_from_event VARCHAR
 );
 
+-- series
 CREATE TABLE IF NOT EXISTS series (
     instance_uid VARCHAR PRIMARY KEY,
     study_instance_uid VARCHAR REFERENCES studies(instance_uid),
     modality VARCHAR,
+    body_part VARCHAR,
     protocol_name VARCHAR,
     series_date DATE,
     series_description VARCHAR,
@@ -40,4 +45,15 @@ CREATE TABLE IF NOT EXISTS series (
     max_submission_timestamp TIME,
     file_size INTEGER,
     third_party_analysis BOOLEAN
+);
+
+-- extractions
+CREATE TABLE IF NOT EXISTS extraction (
+    id SERIAL PRIMARY KEY,
+    image_number INTEGER,
+    series_uid VARCHAR REFERENCES series(instance_uid),
+    feature_name VARCHAR,
+    standardized_feature_name VARCHAR,
+    vocabulary VARCHAR,
+    value FLOAT
 );
