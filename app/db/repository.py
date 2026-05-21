2ac08a8f51ab092bcd822a20668bd98a3e0687ad
steve <steven.mor@mail.polimi.it>
2026-05-21 14:50:56 +0200

    Updated queries

--- app/db/repository.py
+++ app/db/repository.py
@@ -6,6 +6,9 @@ from app.db.models import Study
 from app.db.models import Patient
 from app.db.models import Series
 
+# SET OPRATIONS
+
+
 # this method receives the session object handling all the ORM transactions on our DB and the data related to the object we want to add.
 # If the object is already present we simply return it.
 # Input:
@@ -13,16 +16,17 @@ from app.db.models import Series
 # - collection data in dictionary form
 # Output
 # - obejct that we added or that we found
-def get_or_create_collection(session, data: dict):
-    obj = session.query(Collection).filter_by(name=data['name']).first()
+def create_collection(session, data: dict):
+    obj = session.query(Collection).filter_by(name=data["name"]).first()
 
     if obj:
         return obj
-    
-    obj = Collection(**data) # Python argument unpacking
+
+    obj = Collection(**data)  # Python argument unpacking
     session.add(obj)
     session.commit()
     return obj
+
 
 # This method adds a study to the DB.
 # If the study is already present we simply return it.
@@ -31,16 +35,17 @@ def get_or_create_collection(session, data: dict):
 # - study data in dictionary form
 # Output
 # - obejct that we added or that we found
-def get_or_create_study(session, data: dict):
-    obj = session.query(Study).filter_by(instance_uid=data['instance_uid']).first()
+def create_study(session, data: dict):
+    obj = session.query(Study).filter_by(instance_uid=data["instance_uid"]).first()
 
     if obj:
         return obj
-    
+
     obj = Study(**data)
     session.add(obj)
     session.commit()
     return obj
+
 
 # This method adds a patient to the DB.
 # If the patient is already present we simply return it.
@@ -49,16 +54,17 @@ def get_or_create_study(session, data: dict):
 # - patient data in dictionary form
 # Output
 # - obejct that we added or that we found
-def get_or_create_patient(session, data: dict):
-    obj = session.query(Patient).filter_by(id=data['id']).first()
+def create_patient(session, data: dict):
+    obj = session.query(Patient).filter_by(id=data["id"]).first()
 
     if obj:
         return obj
-    
+
     obj = Patient(**data)
     session.add(obj)
     session.commit()
     return obj
+
 
 # This method adds a series to the DB.
 # If the series is already present we simply return it.
@@ -67,13 +73,46 @@ def get_or_create_patient(session, data: dict):
 # - series data in dictionary form
 # Output
 # - obejct that we added or that we found
-def get_or_create_series(session, data: dict):
-    obj = session.query(Series).filter_by(instance_uid=data['instance_uid']).first()
+def create_series(session, data: dict):
+    obj = session.query(Series).filter_by(instance_uid=data["instance_uid"]).first()
 
     if obj:
         return obj
-    
+
     obj = Series(**data)
     session.add(obj)
     session.commit()
     return obj
+
+
+# GET OPRATIONS
+
+
+def get_studies(session):
+    return session.query(Study).all()
+
+
+def get_all_series(session):
+    return session.query(Series).all()
+
+
+def get_patients(session):
+    return session.query(Patient).all()
+
+
+def get_collections(session):
+    return session.query(Collection).all()
+
+
+def get_series_on_collection(session, collection_name):
+    return (
+        session
+        .query(Series)
+        .join(Series.study)
+        .filter_by(Study.collection == collection_name)
+        .all()
+    )
+
+
+def get_series_on_study(session, study_uid):
+    return session.query(Series).filter_by(study_instance_uid=study_uid).all()
